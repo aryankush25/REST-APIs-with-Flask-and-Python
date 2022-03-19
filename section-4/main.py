01d7f0605187dde34b6f93e2979675045d2b1f6f
Aryan Agarwal <aryankush025@gmail.com>
2022-03-20 02:50:04 +0530

    Section 4

---
+++ section-4/main.py
@@ -0,0 +1,58 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import identity, authenticate
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+api = Api(app)
+
+JWT = JWT(app, authenticate, identity)  # /auth
+
+items = []
+
+
+class Item(Resource):
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    @jwt_required()
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {"message": f'An item with name {name} already exists.'}, 400
+
+        data = request.get_json(silent=True)
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted.'}, 200
+
+    @jwt_required()
+    def put(self, name):
+        data = request.get_json(silent=True)
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item, 200
+
+
+class ItemList(Resource):
+    @jwt_required()
+    def get(self):
+        return {'items': items}, 200
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=8080, debug=True)
