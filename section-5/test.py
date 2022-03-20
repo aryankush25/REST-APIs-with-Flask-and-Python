21089b079d4d1d494a0697b7f02f29f91ec742fa
Aryan Agarwal <aryankush025@gmail.com>
2022-03-21 04:28:47 +0530

    changes in test script

--- section-5/test.py
+++ section-5/test.py
@@ -4,18 +4,18 @@ connection = sqlite3.connect('section-5/data.sqlite')
 
 cursor = connection.cursor()
 
-create_table = 'CREATE TABLE users (id INT, username TEXT, password TEXT);'
+create_table = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT);'
 
 cursor.execute(create_table)
 
-user = (1, 'Aryan', 'test@123')
-insert_query = "INSERT INTO users VALUES (?, ?, ?);"
+user = ('Aryan', 'test@123')
+insert_query = "INSERT INTO users VALUES (NULL, ?, ?);"
 cursor.execute(insert_query, user)
 
 users = [
-    (1, 'Kush', 'test@123'),
-    (2, 'Rahul', 'test@123'),
-    (3, 'Honey', 'test@123'),
+    ('Kush', 'test@123'),
+    ('Rahul', 'test@123'),
+    ('Honey', 'test@123'),
 ]
 
 cursor.executemany(insert_query, users)
