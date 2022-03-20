b122bb7868c00036b0b660a9e59525ff8f0220e0
Aryan Agarwal <aryankush025@gmail.com>
2022-03-21 04:29:05 +0530

    items table and auto increment id added

--- section-5/create_tables.py
+++ section-5/create_tables.py
@@ -4,9 +4,15 @@ connection = sqlite3.connect('section-5/data.sqlite')
 
 cursor = connection.cursor()
 
-create_table = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT);'
+create_users_table = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT);'
 
-cursor.execute(create_table)
+cursor.execute(create_users_table)
+
+create_items_table = 'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL);'
+
+cursor.execute(create_items_table)
+
+cursor.execute("INSERT INTO items VALUES (NULL, 'test', 10.78)")
 
 connection.commit()
 
