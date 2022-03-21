8f358e803ca459e8b47e9e25a735db28c1b82430
Aryan Agarwal <aryankush025@gmail.com>
2022-03-22 03:25:15 +0530

    REST methods for items added items

--- section-5/item.py
+++ section-5/item.py
@@ -10,8 +10,8 @@ class Item(Resource):
     parser.add_argument('price', type=float, required=True,
                         help="This field can not be left blank")
 
-    @jwt_required()
-    def get(self, name):
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.sqlite')
         cursor = connection.cursor()
 
@@ -21,42 +21,100 @@ class Item(Resource):
         row = result.fetchone()
         connection.close()
 
-        if (row):
-            return {"item": {'id': row[0], 'name': row[1], 'price': row[2]}}, 200
+        if row:
+            return {"item": {'id': row[0], 'name': row[1], 'price': row[2]}}
+
+    @classmethod
+    def insert_item(cls, name, price):
+        item = {'name': name, 'price': price}
+
+        connection = sqlite3.connect('data.sqlite')
+        cursor = connection.cursor()
+
+        query = 'INSERT INTO items VALUES (NULL, ?, ?);'
+
+        cursor.execute(query, (item['name'], item['price']))
+        connection.commit()
+        connection.close()
+
+        return item
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+
+        if (item):
+            return item, 200
         else:
             return {"message": "Item not found"}, 404
 
     @jwt_required()
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        if self.find_by_name(name):
             return {"message": f'An item with name {name} already exists.'}, 400
 
         data = Item.parser.parse_args()
 
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
+        item = self.insert_item(name, data['price'])
+
         return item, 201
 
     @jwt_required()
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        if self.find_by_name(name) is None:
+            return {"message": f'An item with name {name} does not exists.'}, 400
+
+        connection = sqlite3.connect('data.sqlite')
+        cursor = connection.cursor()
+
+        query = 'DELETE FROM items WHERE name=?;'
+
+        cursor.execute(query, (name,))
+        connection.commit()
+        connection.close()
+
         return {'message': 'Item deleted.'}, 200
 
     @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            item = self.insert_item(name, data['price'])
         else:
-            item.update(data)
+            item = {'name': name, 'price': data['price']}
+
+            connection = sqlite3.connect('data.sqlite')
+            cursor = connection.cursor()
+
+            query = 'UPDATE items SET price=? WHERE name=?;'
+
+            cursor.execute(query, (item['price'], item['name']))
+            connection.commit()
+            connection.close()
+
         return item, 200
 
 
 class ItemList(Resource):
     @jwt_required()
     def get(self):
+        connection = sqlite3.connect('data.sqlite')
+        cursor = connection.cursor()
+
+        query = 'select * from items;'
+
+        result = cursor.execute(query)
+
+        items = []
+
+        for row in result:
+            items.append(
+                {"item": {'id': row[0], 'name': row[1], 'price': row[2]}}
+            )
+
+        connection.commit()
+        connection.close()
+
         return {'items': items}, 200
